777d00f5e2722abddd6fa32b91e7083d9bfb6ce8
Pedro Monteiro <pedro.monteiro@wansoft.com.br>
2022-12-26 16:57:22 -0300

    Final commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# DT Money
+
+Aplicação desenvolvida na **Trilha ReactJS** do **Ignite**.
+
+Esse projeto simula um app para controle de gastos, sendo possível registrar entradas e saídas de valores, além disso é possível verificar o saldo atual e buscar por transações. Para o desenvolvimento dessa aplicação foi utilizado um guia de estilo para implementar o tema no Styled Components a fim de manter um padrão em todo o app. Além disso foram utilizados: Radix UI, JSON Server, Axios, reduce, contexts, hooks e useMemo.
+
+📱 Figma: https://www.figma.com/file/rZApunLCVXIp2oR2BKZcZ1/DT-Money-(Community)?node-id=0%3A1&t=Pl0PpfUm4FMtvGkk-0
+
+## Executar aplicação
+
+Inicialmente é necessário clonar esse repositório para sua máquina, utilizando o seguinte comando: 
+
+```
+git clone https://github.com/pemonter/dt-money.git
+```
+
+Após terminado o clone do projeto, siga os seguintes passos no terminal:
+```
+cd dt-money
+npm i
+```
+
+Dessa forma os pacotes necessários para execução do projeto estarão instalados via npm.
+
+Para proceder a execução execute o seguinte comando:
+
+``` 
+npm run dev
+```
+
+Então será apresentado o endereço localhost para acesso ao projeto:
+
+http://localhost:5173/
---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,46 @@
+import { useTheme } from 'styled-components'
+
+import { SummaryCard, SummaryContainer } from './styles'
+
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
+
+import { useSummary } from '../../hooks/useSummary'
+
+import { priceFormatter } from '../../utils/formatter'
+
+export function Summary() {
+  const theme = useTheme()
+
+  const summary = useSummary()
+
+  return (
+    <SummaryContainer>
+      <SummaryCard>
+        <header>
+          <span>Entradas</span>
+          <ArrowCircleUp size={32} color={theme['green-300']} />
+        </header>
+
+        <strong>{priceFormatter.format(summary.income)}</strong>
+      </SummaryCard>
+
+      <SummaryCard>
+        <header>
+          <span>Saídas</span>
+          <ArrowCircleDown size={32} color={theme['red-300']} />
+        </header>
+
+        <strong>{priceFormatter.format(summary.outcome)}</strong>
+      </SummaryCard>
+
+      <SummaryCard variant="green">
+        <header>
+          <span>Total</span>
+          <CurrencyDollar size={32} color={theme.white} />
+        </header>
+
+        <strong>{priceFormatter.format(summary.total)}</strong>
+      </SummaryCard>
+    </SummaryContainer>
+  )
+}
